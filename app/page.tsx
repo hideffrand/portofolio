50c9922d3c3d6f73e3518e5c68430f86d1186f23
hideffrand <hideffrand@gmail.com>
2025-05-05 13:26:53 +0700

    fix: connect links

--- app/page.tsx
+++ app/page.tsx
@@ -29,17 +29,17 @@ const Page = () => {
       </h1>
       <div className="relative pb-32 flex flex-col justify-between items-center">
         <div className="flex md:gap-16 gap-10">
-          <a href="mailto:hideffrand@gmail.com" className="md:w-20 md:h-20 w-14 h-14 translate-y-12 hover:cursor-pointer">
+          <a href="mailto:hideffrand@gmail.com" className="md:w-20 md:h-20 w-14 h-14 translate-y-12 hover:translate-y-10 hover:cursor-pointer transition-all">
             <Image className="w-full" src="/gmail.svg" height={800} width={800} alt="Gmail" priority />
           </a>
-          <a href="https://www.linkedin.com/in/deffrand-farera/" className="md:w-20 md:h-20 w-14 h-14 hover:cursor-pointer">
+          <a href="https://www.linkedin.com/in/deffrand-farera/" className="md:w-20 md:h-20 w-14 h-14 hover:-translate-y-2 hover:cursor-pointer transition-all">
             <Image className="w-full" src="/linkedin.svg" height={800} width={800} alt="LinkedIn" priority />
           </a>
-          <a href="https://www.github.com/hideffrand" className="md:w-20 md:h-20 w-14 h-14 translate-y-12 hover:cursor-pointer">
+          <a href="https://www.github.com/hideffrand" className="md:w-20 md:h-20 w-14 h-14 translate-y-12 hover:translate-y-10 hover:cursor-pointer transition-all">
             <Image className="w-full" src="/github.svg" height={800} width={800} alt="Github" priority />
           </a>
         </div>
-        <Image className="absolute bottom-0" src="/beam.svg" width={800} height={800} alt="Beams" />
+        <Image className="absolute bottom-0 -z-10" src="/beam.svg" width={800} height={800} alt="Beams" />
       </div>
     </>
   );
