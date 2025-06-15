97bb31b505a1a9cd1cc0a46763ed5f796bff869b
deffrand <deffrand.farera@dataon.com>
2025-06-15 12:30:20 +0700

    fix: nav icon size

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,18 +9,18 @@ interface ILinks {
 }
 
 const links: ILinks[] = [
-  { href: "#home", label: <FaHouse /> },
-  { href: "#things-i-do", label: <FaLaptopCode /> },
-  { href: "#experience", label: <FaBusinessTime /> },
-  { href: "#works", label: <GoProjectRoadmap /> },
-  { href: "#contact", label: <IoMdCall /> },
+  { href: "#home", label: <FaHouse size={26} /> },
+  { href: "#things-i-do", label: <FaLaptopCode size={30} /> },
+  { href: "#experience", label: <FaBusinessTime size={30} /> },
+  { href: "#works", label: <GoProjectRoadmap size={30} /> },
+  { href: "#contact", label: <IoMdCall size={30} /> },
 ]
 
 export default function Navbar() {
   return (
     <nav className="flex items-center justify-between md:justify-center gap-6 px-6 py-4 md:py-6 fixed w-full md:w-fit z-40 top-0 right-1/2 translate-x-1/2 bg-[rgb(0,0,0,0.9)] border border-[rgb(200,200,200,0.2)] bg-blur-120 md:rounded-b-[10px]">
       {links.map((link, i) => (
-        <a key={i} href={link.href} className="text-3xl hover:text-white text-[rgb(120,120,120)]">{link.label}</a>
+        <a key={i} href={link.href} className="hover:text-white text-[rgb(120,120,120)]">{link.label}</a>
       ))}
     </nav>
   )
