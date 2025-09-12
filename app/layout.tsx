af8092b7f0a57fee5100ff817c887a497d327a9c
hideffrand <hideffrand@gmail.com>
2025-09-12 22:23:43 +0700

    fix: refactor navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import FollowingDiv from "./components/FollowingDiv";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,9 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Navbar />
         <div className="base-bg w-full h-screen fixed"></div>
         <FollowingDiv />
-
         <div className="max-w-[1024px] min-w-full md:px-[12%] px-4">{children}</div>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,11 @@ import ThingsIDo from "./components/ThingsIDo";
 import SelectedWork from "./components/SelectedWork";
 import Hero from "./components/Hero";
 import Experience from "./components/Experience";
-import Navbar from "@/components/Navbar";
 import Footer from "./components/Footer";
 
 const Page = () => {
   return (
     <>
-      <Navbar />
       <Hero />
       <ThingsIDo />
       <Experience />
